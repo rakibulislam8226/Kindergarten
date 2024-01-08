27c1b5400d831ffe14ee2b1a136e71cec019bcce
rakibulislam8226 <rakibulislam8226@gmail.com>
2024-01-08 14:38:49 +0600

    Added post request at classwork plan

--- frontend/src/Components/Classworks/Plan/Plan.jsx
+++ frontend/src/Components/Classworks/Plan/Plan.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+
+import { toast } from 'react-toastify';
 import axios from 'axios';
 
 const Plan = () => {
     const [plans, setPlans] = useState([]);
+    const [newPlan, setNewPlan] = useState('');
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/v1/classwork/plan')
@@ -14,10 +18,45 @@ const Plan = () => {
             });
     }, []);
 
+    const handleInputChange = (e) => {
+        setNewPlan(e.target.value);
+        setError(null);
+    };
+
+    const handleAddPlan = () => {
+        if (newPlan.trim() !== '') {
+            axios.post('http://127.0.0.1:8000/api/v1/classwork/plan', { items: newPlan })
+                .then(response => {
+                    setPlans([...plans, response.data]);
+                    setNewPlan('');
+                    
+                    // Show success toast message
+                    toast.success('Plan created successfully!', {
+                        position: 'top-right',
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                    });
+                })
+                .catch(error => {
+                    console.error('Error adding plan:', error);
+                });
+        } else {
+            setError('Please enter a plan.');
+        }
+    };
 
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-3xl font-bold mb-4">Plans List</h1>
+
+            <div className="mb-4">
+                <input type="text" placeholder="Enter a new plan" value={newPlan} onChange={handleInputChange} className="border border-gray-300 p-2 mr-2" />
+                <button onClick={handleAddPlan} className="bg-blue-500 text-white p-2 rounded-md"> Add Plan </button>
+                {error && <p className="text-red-500">{error}</p>}
+            </div>
 
             {plans.map(plan => (
                 <div key={plan.uid} className="bg-white p-4 shadow-md mb-4 rounded-md">
@@ -31,7 +70,6 @@ const Plan = () => {
             )}
         </div>
     );
-
 }
 
 export default Plan;
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -10,35 +10,35 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-gray-800 text-white p-4">
+        <nav className={`bg-gray-800 text-white p-4 transition-color ${isNavOpen ? 'duration-500' : 'duration-300'}`}>
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-lg font-semibold">
-                    <Link to="/" className="hover:text-gray-300 text-2xl">Kinder <span className='text-orange-500'>Garten</span></Link>
+                    <Link to="/" className="hover:text-gray-300 text-2xl lg:text-4xl">Kinder<span className='text-orange-500'>Garten</span></Link>
                 </div>
 
                 {/* Mobile Navigation Toggle */}
-                <AiOutlineMenu size={25} onClick={toggleNav} className={`transition-transform ${isNavOpen ? 'transform rotate-180 duration-500' : 'duration-500'}`} />
+                <AiOutlineMenu size={25} onClick={toggleNav} className={`transition-transform cursor-pointer ${isNavOpen ? 'transform rotate-180 duration-500' : 'duration-500'}`} />
 
             </div>
 
             {/* Modal Navigation */}
             <div className={`fixed top-0 right-0 h-full w-1/3 bg-gray-800 text-white p-4 transform ${isNavOpen ? 'translate-x-0 opacity-100 duration-500' : 'translate-x-full opacity-0 duration-300'}`}>
                 <div>
-                    <AiOutlineClose size={25} onClick={toggleNav} className="text-white hover:text-gray-300 float-right transition-transform duration-300" />
-                    <h2 className='hidden lg:flex font-bold text-2xl'>Kinder<span className='text-orange-500'>Garten</span></h2>
+                    <AiOutlineClose size={25} onClick={toggleNav} className="text-white hover:text-gray-300 float-right transition-transform duration-300 cursor-pointer" />
+                    <Link to="/" className="hover:text-gray-300 text-3xl font-bold">Kinder<span className='text-orange-500'>Garten</span></Link>
                 </div>
                 <ul className="flex flex-col space-y-4 mt-12 font-semibold text-xl">
                     <li>
-                        <Link to="/" className="hover:text-gray-300">Home</Link>
+                        <Link to="/" className="hover:text-gray-300 cursor-pointer">Home</Link>
                     </li>
                     <li>
-                        <Link to="#" className="hover:text-gray-300">About</Link>
+                        <Link to="#" className="hover:text-gray-300 cursor-pointer">About</Link>
                     </li>
                     <li>
-                        <Link to="/plan" className="hover:text-gray-300">Plan</Link>
+                        <Link to="/plan" className="hover:text-gray-300 cursor-pointer">Plan</Link>
                     </li>
                     <li>
-                        <Link to="#" className="hover:text-gray-300">Contact</Link>
+                        <Link to="#" className="hover:text-gray-300 cursor-pointer">Contact</Link>
                     </li>
                 </ul>
             </div>
